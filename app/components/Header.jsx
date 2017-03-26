c214f0b2b42f10d43926b5af7803125cc655bc26
pashebor <pashebor@gmail.com>
2017-03-26 23:48:27 +0300

    Header content area has been added

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -2,15 +2,14 @@ import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from './NavBar.jsx';
+import HeaderContent from './HeaderContent';
 
 class Header extends Component{
     render() {
         return(
             <header className="header">
                 <NavBar/>
-               <section className="content header--background">
-
-               </section>
+                <HeaderContent/>
             </header>
         )
     }
---
+++ app/components/HeaderContent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class HeaderContent extends Component{
+    render() {
+        return(
+            <section className="header-content-background">
+                <div className="content--background">
+                    <div className="content__space"></div>
+                    <div className="container header__content">
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                        <p>sdfgsdfsdfds</p>
+                    </div>
+                    <div className="content__space"></div>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default HeaderContent;
