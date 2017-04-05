ac9b538b212ba80ad22e1804a2e072dacdeec4c6
pashebor <pashebor@gmail.com>
2017-04-05 12:04:32 +0300

    All borders from blocks and headers have been removed

--- app/components/YouSiteAudit.js
+++ app/components/YouSiteAudit.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
+import {showModal} from '../actions/index';
+import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux';
 
-const YouSiteAudit = () =>{
+class YouSiteAudit extends Component{
+    callPopupHandler() {
+        this.props.showModal(true);
+    }
+    render() {
         return(
             <section className="audit">
                 <div className="container">
                     <h2 className="audit__title">Аудит Вашего сайта поможет</h2>
                     <div className="audit-info">
                         <div className="audit-info__item">
-                            <img src="images/schedule.svg" alt="Позиции" title="Поднять позиции"/>
+                            <img src="images/schedule.svg" alt="Позиции" title="Поднять позиции" onClick={this.callPopupHandler.bind(this)}/>
                             <h4>Поднять позиции</h4>
                             <p>в поисовых системах</p>
                         </div>
                         <div className="audit-info__item">
-                            <img src="images/clients.svg" alt="Позиции" title="Поднять позиции"/>
+                            <img src="images/clients.svg" alt="Позиции" title="Поднять позиции" onClick={this.callPopupHandler.bind(this)}/>
                             <h4>Увеличить</h4>
                             <p>число новых клиентов</p>
                         </div>
                         <div className="audit-info__item">
-                            <img src="images/lowprice.svg" alt="Позиции" title="Поднять позиции"/>
+                            <img src="images/lowprice.svg" alt="Позиции" title="Поднять позиции" onClick={this.callPopupHandler.bind(this)}/>
                             <h4>Уменьшить стоимость</h4>
                             <p>привлечения клиентов</p>
                         </div>
@@ -25,6 +32,17 @@ const YouSiteAudit = () =>{
                 </div>
             </section>
         );
+    }
+}
+
+const mapStateToProps = (store) => {
+    return {
+        formState: store.salesReducer
+    }
 };
 
-export default YouSiteAudit;
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({showModal}, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(YouSiteAudit);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -36,7 +36,7 @@ module.exports = {
         filename: '[name].js'
     },
 
-    watch: true,
+    watch: false,
 
     module: {
         loaders: [
